233691617c8d776d045413489c42d05616826904
Letícia Sampaio <leticia.sampaio.fagundes@gmail.com>
2025-05-22 19:46:22 -0300

    Update DeleteTasksController.cs

--- Controllers/DeleteTasksController.cs
+++ Controllers/DeleteTasksController.cs
@@ -1,23 +1,3 @@
-// using TodoList.Models; 
-
-// public static class Rota_DELETE
-// {
-//     public static void MapDeleteRoutes(this WebApplication app)
-//     {
-//         app.MapDelete("/tarefas/{id}", async (int id, AppDbContext context) =>
-//         {
-//             var tarefa = await context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
-
-//             if (tarefa == null)
-//                 return Results.BadRequest("Tarefa não encontrada.");
-
-//             context.Tasks.Remove(tarefa);
-//             await context.SaveChangesAsync();
-
-//             return Results.Ok("Tarefa deletada com sucesso.");
-//         });
-//     }
-// }
 using System;
 using System.Linq;
 using TodoList.Data;
