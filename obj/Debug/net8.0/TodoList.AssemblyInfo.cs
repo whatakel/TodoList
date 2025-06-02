7325589fffd4d5cbd2dbfec9369c8cd33dcb51ea
Letícia Sampaio <leticia.sampaio.fagundes@gmail.com>
2025-06-02 20:57:08 -0300

    ajuste registros

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a06316e90ac1dca8d88215461d8b7e68f78df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a0f6ab3f8e9ce1385420b43f62eb09cd3e85a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
